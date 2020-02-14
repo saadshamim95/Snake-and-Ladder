c523c46fbe4ecad68394cadd0272971fe82451b3
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-02-14 18:34:39 +0530

    [Saad] Refactor. adding optons of no play, snake and ladder

--- Snake_Ladder.sh
+++ Snake_Ladder.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+Player1=0
+dice=$$
+rollDice(){
+	dice=$(( $((RANDOM%6))+1 ))
+	echo "Dice: $dice"
+}
+
+option=$$
+checkOptions(){
+	option=$((RANDOM%3))
+	if((option==0))
+	then
+		echo "No Play"
+		Player1=$Player1
+	elif((option==1))
+	then
+		echo "Ladder"
+		Player1=$((Player1+dice))
+	else
+		echo "Snake"
+		Player1=$((Player1-dice))
+	fi
+}
+
+rollDice
+checkOptions
+echo "Player1: $Player1"
