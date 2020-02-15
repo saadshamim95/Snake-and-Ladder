8123dbcadfb2a944001a3aad70594c9a5a61c7d2
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-02-15 11:52:44 +0530

    [Saad] Refactor. adding number of times dice rolled

--- Snake_Ladder.sh
+++ Snake_Ladder.sh
@@ -1,1 +1,42 @@
 #!/bin/bash
+Player1=0
+dice=$$
+rollDice(){
+	dice=$(( $((RANDOM%6))+1 ))
+	echo "Dice: $dice"
+}
+
+option=$$
+checkOptions(){
+	option=$((RANDOM%3))
+	if((option==0))
+	then
+		echo "No Play"
+		Player1=$Player1
+	elif((option==1))
+	then
+		echo "Ladder"
+		Player1=$((Player1+dice))
+	else
+		echo "Snake"
+		Player1=$((Player1-dice))
+	fi
+}
+
+count=0
+while((Player1<100))
+do
+	rollDice
+	checkOptions
+	if((Player1<0))
+	then
+		Player1=0
+	fi
+	if((Player1>100))
+	then
+		Player1=$((Player1-dice))
+	fi
+	echo "Player1: $Player1"
+	count=$((count+1))
+done
+echo "Total number of times Dice rolled: $count"
