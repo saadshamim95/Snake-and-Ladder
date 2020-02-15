bdd1cd72b9bfb878a4abb042fab8f29045bafac5
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-02-15 11:17:35 +0530

    [Saad] Refactor. Adding winning condition to be 100 and player can't go past 100

--- Snake_Ladder.sh
+++ Snake_Ladder.sh
@@ -1,1 +1,39 @@
 #!/bin/bash
+Player1=0
+dice=$$
+rollDice(){
+	dice=$(( $((RANDOM%6))+1 ))
+	echo "Dice: $dice"
+}
+
+option=$$
+checkOptions(){
+	option=$((RANDOM%3))
+	if((option==0))
+	then
+		echo "No Play"
+		Player1=$Player1
+	elif((option==1))
+	then
+		echo "Ladder"
+		Player1=$((Player1+dice))
+	else
+		echo "Snake"
+		Player1=$((Player1-dice))
+	fi
+}
+
+while((Player1<100))
+do
+	rollDice
+	checkOptions
+	if((Player1<0))
+	then
+		Player1=0
+	fi
+	if((Player1>100))
+	then
+		Player1=$((Player1-dice))
+	fi
+	echo "Player1: $Player1"
+done
